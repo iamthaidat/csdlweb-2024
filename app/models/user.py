99a30316a32427b1f713f420ab0f8f42c55eed13
Dương Nguyễn Việt Hải <viethai1442003@gmail.com>
2025-01-15 15:16:22 +0700

    Delete app directory

--- app/models/user.py
+++
@@ -1,4 +0,0 @@
-# from sqlalchemy import MetaData, Column, Integer, JSON, String, Table, ForeignKey, DateTime, Boolean, Float, Time
-# from datetime import datetime
-#
-# from .delivery import metadata
--- app/services/admin_service.py
+++
@@ -1,8 +0,0 @@
-from fastapi import Depends
-
-from dependencies import fastapi_users
-from models.delivery import User
-
-
-def get_current_user(user: User = Depends(fastapi_users.current_user())):
-    return user
